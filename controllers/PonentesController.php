11d5be08800430e38569dc80ec5c29763e42dfc1
JSanchezs024 <joaquin.sanchez.suazo@gmail.com>
2024-12-21 23:20:15 -0300

    avances v.1.4

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -3,6 +3,8 @@
 namespace Controllers;
 
 use MVC\Router;
+use Model\Ponente;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class PonentesController {
 
@@ -13,11 +15,57 @@ class PonentesController {
     }
     public static function crear(Router $router){
         $alertas = [];
+        $ponente = new Ponente;
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            //leer imagen
+            if(!empty($_FILES['imagen']['tmp_name'])){
+                
+                $carpeta_imagenes = '../public/img/speakers';
+
+                //Crear la carpeta si no existe
+                if(!is_dir($carpeta_imagenes)){
+                    mkdir($carpeta_imagenes, 0777, true);
+                }
+
+                $imagen_png = Image::make($_FILES['imagen']['tmp_name'])->fit(800, 800)->encode('png', 80);
+                $imagen_webp = Image::make($_FILES['imagen']['tmp_name'])->fit(800, 800)->encode('webp', 80);
+
+                $nombre_imagen = md5(uniqid( rand(), true));
+
+                $_POST['imagen'] = $nombre_imagen;
+
+            }
+
+            $_POST['redes'] = json_encode($_POST['redes'], JSON_UNESCAPED_SLASHES);
+        
+            $ponente->sincronizar($_POST);
+            debuguear($ponente);
+            
+            //debuguear(json_encode($_POST['redes'], JSON_UNESCAPED_SLASHES));
+            //validar
+            $alertas = $ponente->validar();
+
+            //Guardar el registro
+            if(empty($alertas)){
+                //Guardar las imagenes
+                $imagen_png->save($carpeta_imagenes . '/' . $nombre_imagen . ".png");
+                $imagen_webp->save($carpeta_imagenes . '/' . $nombre_imagen . ".webp");
+
+                //Guardar en la BD
+                $resultado = $ponente->guardar();
+
+                if($resultado){
+                    header('location: /admin/ponentes');
+                }
+            }
+        }
 
 
         $router->render('admin/ponentes/crear',[
             'titulo' => 'Registrar Ponente',
-            'alertas' => $alertas
+            'alertas' => $alertas,
+            'ponente' => $ponente
         ]);
     }
 }
